aaf680a69954a24aa05d4a32101a707ea64cd96b
Mathis <87679380+Mathiiis@users.noreply.github.com>
2024-06-26 16:21:50 -0400

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import Toolbar from './components/Toolbar';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <Toolbar />
+
+    </div>
+  );
+}
+
+export default App;
