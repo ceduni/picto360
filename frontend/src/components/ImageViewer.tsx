53e2a6fe5b2249b0779a5db486bd11313fca473e
TBAce11 <tbaghilas@gmail.com>
2024-07-05 20:35:30 -0400

    Basic context menu implementation
    - Submenus to be added shortly
    - Separation of the code in a new component in progress

--- frontend/src/components/ImageViewer.tsx
+++ frontend/src/components/ImageViewer.tsx
@@ -1,6 +1,12 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from "react";
 import { MdOutlineFileDownload } from "react-icons/md";
-import './ImageViewer.css';
+import {
+  AiOutlineForm,
+  AiOutlineFileText,
+  AiOutlineLink,
+  AiOutlinePicture,
+} from "react-icons/ai";
+import "./ImageViewer.css";
 
 declare global {
   interface Window {
@@ -10,24 +16,65 @@ declare global {
 
 const ImageViewer: React.FC = () => {
   const viewerElement = useRef<HTMLDivElement>(null);
-  const [imageSrc, setImageSrc] = useState('');
+  const [imageSrc, setImageSrc] = useState("");
   const [showPopup, setShowPopup] = useState(true);
+  const [contextMenu, setContextMenu] = useState({
+    visible: false,
+    x: 0,
+    y: 0,
+  });
+  const contextMenuRef = useRef<HTMLDivElement>(null);
 
-  // Fenetre image360°
+  // Initialize the pannellum viewer (360° image window)
   useEffect(() => {
-    if (viewerElement.current  && imageSrc) {
+    if (viewerElement.current && imageSrc) {
       window.pannellum.viewer(viewerElement.current, {
-        type: 'equirectangular',
+        type: "equirectangular",
         panorama: imageSrc,
         autoLoad: true,
         autoRotate: -2,
         showZoomCtrl: false,
-
       });
     }
   }, [imageSrc]);
 
-  // Permettre le glisser déposer
+  useEffect(() => {
+    const handleContextMenu = (e: Event) => {
+      const customEvent = e as CustomEvent;
+      setContextMenu({
+        visible: true,
+        x: customEvent.detail.x,
+        y: customEvent.detail.y,
+      });
+    };
+
+    window.addEventListener(
+      "showContextMenu",
+      handleContextMenu as EventListener
+    );
+
+    // Event listener to close the context menu when clicking outside of it
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        contextMenuRef.current &&
+        !contextMenuRef.current.contains(event.target as Node)
+      ) {
+        setContextMenu({ ...contextMenu, visible: false });
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      window.removeEventListener(
+        "showContextMenu",
+        handleContextMenu as EventListener
+      );
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [contextMenu]);
+
+  // Drag and drop logic for image upload
   const handleDragOver = (e: React.DragEvent) => {
     e.preventDefault();
   };
@@ -42,6 +89,7 @@ const ImageViewer: React.FC = () => {
     }
   };
 
+  // Handle file input for image upload
   const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files[0]) {
       const file = e.target.files[0];
@@ -51,30 +99,63 @@ const ImageViewer: React.FC = () => {
     }
   };
 
+  // Handle context menu item click
+  const handleContextMenuItemClick = (type: string) => {
+    console.log("Context menu item clicked:", type);
+    // logic to handle the creation of different types of hotspots here
+    setContextMenu({ ...contextMenu, visible: false });
+  };
+
   return (
     <>
       {showPopup && (
         <div className="modal">
           <div className="modal-content">
             <h2>Choisissez une image</h2>
-            <div 
-              className="drop-zone" 
-              onDragOver={handleDragOver} 
+            <div
+              className="drop-zone"
+              onDragOver={handleDragOver}
               onDrop={handleDrop}
-              style={{ border: '2px dashed #ccc', padding: '50px', cursor: 'pointer' }}
-              >
-                <p id='download-logo'><MdOutlineFileDownload /></p>
-              <p>Déposez votre image ici ou cliquez pour sélectionner une image</p>
+              style={{
+                border: "2px dashed #ccc",
+                padding: "50px",
+                cursor: "pointer",
+              }}
+            >
+              <p id="download-logo">
+                <MdOutlineFileDownload />
+              </p>
+              <p>
+                Déposez votre image ici ou cliquez pour sélectionner une image
+              </p>
             </div>
             <input type="file" onChange={handleImageChange} />
-            
           </div>
         </div>
       )}
-      <div
-        ref={viewerElement}
-        className="viewer-container"
-      ></div>
+      <div ref={viewerElement} className="viewer-container"></div>
+      {contextMenu.visible && (
+        <div
+          ref={contextMenuRef}
+          className="context-menu"
+          style={{ top: `${contextMenu.y}px`, left: `${contextMenu.x}px` }}
+        >
+          <ul>
+            <li onClick={() => handleContextMenuItemClick("Form")}>
+              <AiOutlineForm className="menu-icon" /> Formulaire
+            </li>
+            <li onClick={() => handleContextMenuItemClick("Text")}>
+              <AiOutlineFileText className="menu-icon" /> Texte
+            </li>
+            <li onClick={() => handleContextMenuItemClick("Link")}>
+              <AiOutlineLink className="menu-icon" /> Lien
+            </li>
+            <li onClick={() => handleContextMenuItemClick("Media")}>
+              <AiOutlinePicture className="menu-icon" /> Média
+            </li>
+          </ul>
+        </div>
+      )}
     </>
   );
 };
