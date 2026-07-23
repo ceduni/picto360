f84777515cb1258e39e837c3b15d55e1a7d743fc
Pejjp <jeffpendy33@gmail.com>
2026-07-23 12:21:34 -0400

    OLpermission model

---
+++ backend/src/models/user_perm.model.ts
@@ -0,0 +1,59 @@
+import mongoose, { Document, Schema, Types } from "mongoose";
+
+export type PermissionAction = "VIEW" | "EDIT" | "UPLOAD" | "DELETE";
+export type PermissionResourceType =
+  | "PictoProject"
+  | "PictoImage"
+  | "Annotation"
+  | "Content"
+  | "Activity";
+type PermissionSubjectType = "USER" | "PARTICIPANT"
+
+interface PermissionDocument extends Document{
+    subjectType : PermissionSubjectType
+    subjectId: Types.ObjectId
+    objectType: PermissionResourceType,
+    objectId: Types.ObjectId;
+    actions:[PermissionAction],
+}
+
+const permissionSchema = new Schema<PermissionDocument>(
+  {
+    subjectType: {
+      type: String,
+      enum: ["USER", "PARTICIPANT"],
+      required: true,
+    },
+    subjectId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      index: true,
+    },
+    objectType: {
+      type: String,
+      enum: ["PictoProject", "PictoImage", "Annotation", "Content", "Activity"],
+      required: true,
+      index: true,
+    },
+    objectId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      index: true,
+    },
+    actions: {
+      type: [String],
+      enum: ["VIEW", "EDIT", "UPLOAD", "DELETE"],
+      required: true,
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+
+permissionSchema.index(
+  { subjectType: 1, subjectId: 1, resourceType: 1, resourceId: 1 },
+  { unique: true }
+);
+const Permission = mongoose.model<PermissionDocument>("Permission", permissionSchema);
+
+export default Permission 
